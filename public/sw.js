755ef660c05469a6efe6cd7daf2df15659d4c2c1
uzomao <chidumaga@gmail.com>
2026-04-02 16:54:42 +0100

    feat: update HTML structure, add service worker, and include manifest for PWA support

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('finance-tracker-v1').then((cache) =>
+      cache.addAll([
+        '/',
+        '/index.html',
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
